93c0b5f47f4092a22e1033565845f8f5b749bcb3
LinneaMay <Linneamay@gilbertson.net>
2024-09-27 20:59:40 -0500

    one more test to understand things better

--- check_whitespace.c
+++ check_whitespace.c
@@ -61,7 +61,7 @@ int is_clean(char const *str) {
   // greater than the second.
   int result = strcmp(str, cleaned);
 
-  if (cleaned != "") {
+  if (cleaned != NULL) {
       free((void *)cleaned);
   }
 
